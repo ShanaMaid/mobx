2fcb5b1f74197e49db931c3316c77faf4d206306
ShanaMaid <uestczeng@gmail.com>
2018-03-19 13:15:21 +0800

    refactor observable

--- src/observable.js
+++ src/observable.js
@@ -1,35 +1,29 @@
-import {isObject} from '../utils/isObject';
-import {deepClone} from '../utils/deepClone';
 
-const globalObjId = 1; // 监听对象全局唯一id
+let globalObjId = 1; // 监听对象全局唯一id
 
-/**
- * observable
- * 阉割版，只支持监听object对象
- */
-export function observable(target) {
-  if (!isObject(target)) {
-    return -1;
+export class Observable{
+  uniqueId = 1; //  全局唯一id
+
+  value = null; // 观测值
+
+  constructor(v) {
+    this.uniqueId = `observer-${globalObjId++}`;
+
+    this.value = v;
   }
-  const obj = deepClone(target);
-  // 遍历对象
-  Object.keys(obj).forEach(function (index) {
-    const key = index;
-    const val = obj[key];
-    observable(val);  // 递归查询建立监听机制
-    Object.defineProperty(obj, key, {
-      enumerable: true,
-      configurable: false,
-      get: function () {
-        return val;
-      },
-      set: function (newVal) {
-        if (newVal === obj[key]) {
-          return;
-        }
-        return -1;
-      }
-    });
-  });
-  return obj;
+
+  /**
+   * getter
+   */
+  get () {
+    return this.value;
+  }
+
+  /**
+   * setter
+   * @param {new} v 
+   */
+  set (v) {
+    this.value = v;
+  }
 }
