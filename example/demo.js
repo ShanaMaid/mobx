0ca8d817b212879c1ea421e236f3d41c469f8a30
ShanaMaid <uestczeng@gmail.com>
2018-03-21 16:57:29 +0800

    优化demo

--- example/demo.js
+++ example/demo.js
@@ -1,10 +1,11 @@
-import { observable, autorun } from "../src/mopx";
+import { observable, autorun, computed } from "../src/mopx";
 
 /**
  * 定义一个人物
- * 具有等级、血量属性
+ * 具有等级、血量、战斗力属性
  * 能够打怪、回血、买物品的能力
  * 有背包能存储东西
+ * 战斗力 = 等级 * 血量
  */
 class Character{
   @observable
@@ -15,6 +16,12 @@ class Character{
 
   @observable
   backpack = ['长剑', '布甲'];
+  
+  @computed get fight () {
+    const result = this.level * this.hp;
+    console.log(`战斗力发生变化，当前战斗力：${result}`);
+    return result;
+  }
 
   killMonster() {
     this.level++;
@@ -48,6 +55,10 @@ const backpackChange = autorun(() => {
   console.log(`shana背包发生变化，当前包裹物品:${shana.backpack.join(',')}`);
 });
 
+// const fightChange = autorun(() => {
+//   console.log(`shana战斗力发生变化，当前战斗力品:${shana.fight}`);
+// });
+
 // 这里我们利用autorun制作了一个插件，这样我们只用打挂升级了，当蓝量和血量低于某个值的时候脚本会自动帮我们恢复
 const plugin = autorun(() => {
   const hp = 10;
@@ -66,16 +77,9 @@ console.log('====================================');
 console.log('以上全是autorun第一次收集依赖时候自定自行的打印信息');
 console.log('====================================');
 
+shana.fight;   // 引用computed使之产生依赖
+
 // 当我们没有插件的时候，我们需要自己打怪升级的同时注意恢复蓝和血
 // shana.killMonster().restoreStatus().killMonster().restoreStatus().killMonster();
-
 // 当使用插件的时候，我们只管一直打怪
-// shana.killMonster().killMonster().killMonster().killMonster().killMonster().killMonster().killMonster().killMonster();
-
-
-
-shana.backpack.push('盾牌');
-
-// shana.backpack = [1,3,4]
-
-// console.log(shana.backpack)
+shana.killMonster().killMonster().killMonster().killMonster().killMonster().killMonster().killMonster().killMonster();
